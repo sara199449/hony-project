32b2def32f2a7bc9bf86c382ac367bad6d1ba622
sara199449 <sartmrad14@gmail.com>
2026-05-03 01:19:23 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,13 @@
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69b344899dd4d71c370f306b/1jji47gap';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
